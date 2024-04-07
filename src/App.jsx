6cc84e17f03aaa67e885b59b17c9c4fbdf173a3b
lynnjaco <jacobs.lynnette@gmail.com>
2024-04-07 12:53:36 -0400

    adds and imports the current score component to display the total amount of points in a header element

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import { CurrentScore } from "./Score";
 
 function App () {
     return (
       <main>
+        <CurrentScore />
         <p>React State Lab</p>
       </main>
     );
---
+++ src/Score.jsx
@@ -0,0 +1,8 @@
+
+export const CurrentScore = () => {
+    return(
+        <div>
+            <h1>Current Score: <span>{  }</span></h1>
+        </div>
+    )
+}
